4c905e8458aefa347478b5c6fbe5994e27744f53
SOTSYS216 <sotsys216@SOTSYS216s-Mac-mini.local>
2018-04-16 15:28:41 +0530

    Initial commit

---
+++ Example/HSRange/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HSRange
+//
+//  Created by Hitendra Solanki on 04/16/2018.
+//  Copyright (c) 2018 Hitendra Solanki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
