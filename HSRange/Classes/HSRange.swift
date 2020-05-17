3131e9159caed5e6b66cb1610556749cb94025c7
Hitendra Solanki <hitendra.developer@gmail.com>
2020-05-17 15:11:18 +0530

    Updated copy right and usage information

--- HSRange/Classes/HSRange.swift
+++ HSRange/Classes/HSRange.swift
@@ -3,7 +3,25 @@
 //  HSRange
 //
 //  Created by Hitendra Solanki on 05/07/17.
-//  Copyright © 2017 Hitendra Solanki. All rights reserved.
+//  Copyright © 2017-2020 Hitendra Solanki. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 import Foundation
@@ -28,7 +46,11 @@ public extension HSRange {
     }
     
     //this returns the value inside of range
-    //e.g. Range(-100,100), if you pass -200, this will return -100
+    //e.g. Range(-100,100),
+    //  if you pass -200, this will return -100
+    //  if you pass 250, this will return 100
+    //  if you pass 80, this will return 80
+    //  if you pass -20, this will return -20
     func innerValue(value: Double) -> Double {
         return min(max(value,self.low),self.high)
     }
